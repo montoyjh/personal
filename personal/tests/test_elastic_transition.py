9923ddb28f5bd0b0c7213c4d939f8b330d54ccb0
montoyjh <montoyjh@gmail.com>
2017-12-04 14:48:00 -0800

    added preliminary test

---
+++ personal/tests/test_elastic_transition.py
@@ -0,0 +1,10 @@
+from unittest2 import unittest
+
+db_filename = 'materials_db_file.yaml'
+
+class ElasticTransitionTest(unittest2.testCase):
+    def setUp(self):
+        pass
+
+    def test_get_structures(self):
+        pass
