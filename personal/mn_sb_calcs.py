791160b94e4b46be45c43b9895a384b8e5908a97
montoyjh <montoyjh@gmail.com>
2017-12-21 14:12:43 -0800

    added template to list and extra

--- personal/mn_sb_calcs.py
+++ personal/mn_sb_calcs.py
@@ -33,7 +33,7 @@ def get_structures_by_template(template, perturbations=1):
             for relative to the template composition
     """
     comp = template.composition.reduced_composition.get_el_amt_dict()
-    all_structures = []
+    all_structures = [template]
     for elt, sub in ['Mn', 'Sb'], ['Sb', 'Mn']:
         # I'm sure there's a more elegant way to do this, sorry to future self
         if elt in comp:
@@ -43,6 +43,8 @@ def get_structures_by_template(template, perturbations=1):
                 combos = itertools.combinations(indices, n)
                 for combo in list(combos):
                     new_structure = template.copy()
+                    if new_structure.num_sites < 7:
+                        new_structure.make_supercell([2, 2, 2])
                     for idx in combo:
                         new_structure[idx] = sub
                         all_structures.append(new_structure)
@@ -51,13 +53,21 @@ def get_structures_by_template(template, perturbations=1):
     for new_struct in tqdm.tqdm(all_structures, desc='reducing structures'):
         if not any([structure_matcher.fit(new_struct, struct) for struct in unique_structs]):
             unique_structs.append(new_struct)
-    assert len(set([s.composition for s in unique_structs])) < 2 * n + 1
+    assert len(set([s.composition for s in unique_structs])) <= 2 * n + 1
     return unique_structs
 
 def get_all_structs_by_material_id(mp_ids):
     return {mpid[0]: get_structures_by_template(
         mpr.get_structure_by_material_id(mpid[0]), mpid[1])
             for mpid in tqdm.tqdm(mp_ids)}
+
+def get_wf(structure, tags = None):
+    wf = wf_structure_optimization(structure)
+    wf.append_wf(wf_static(structure), wf.leaf_fw_ids)
+    wf = add_modify_incar(wf)
+    if tags:
+        wf = add_tags(wf, tags)
+    return wf
 
 if __name__=="__main__":
     structures = get_all_structs_by_material_id([("mp-19231", 2),
@@ -69,14 +79,21 @@ if __name__=="__main__":
                                                  ("mp-2136",  2),
                                                  ("mp-1705",  2),
                                                  ("mp-230", 2)])
-    # all_structures = itertools.chain.from_iterable(structures.values())
+
+    # Also get TiO2 as template
+    rutile = mpr.get_structure_by_material_id("mp-2657")
+    ti_indices = [rutile.index(s) for s in rutile if s.species_string=='Ti']
+    rutile[ti_indices[0]] = 'Mn'
+    rutile[ti_indices[1]] = 'Sb'
+    structures["mp-2657"] = get_structures_by_template(rutile, 2)
+
     lpad = LaunchPad.from_file(sys.argv[1])
+    wfs = []
     for mpid, structures in structures.items():
         for structure in structures:
-            wf = wf_structure_optimization(structure)
-            wf.append_wf(wf_static(structure), wf.leaf_fw_ids)
-            wf = add_modify_incar(wf)
-            wf = add_tags(wf, ["mn_sb_calcs", mpid[0]])
+            wfs.append(get_wf(structure, tags=["mn_sb_calcs_3", mpid]))
+
+    launch = True
+    if launch:
+        for wf in wfs:
             lpad.add_wf(wf)
-    # print("\n".join("{}: {}".format(k, len(v)) for k, v in structures.items()))
-    # import pdb; pdb.set_trace()
